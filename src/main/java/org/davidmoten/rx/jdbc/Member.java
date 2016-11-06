de9b94712b9b7e8b42dbadf9c8ba482a37b09181
David Moten <davidmoten@gmail.com>
2016-11-06 19:13:54 +1100

    dev

--- src/main/java/org/davidmoten/rx/jdbc/Member.java
+++ src/main/java/org/davidmoten/rx/jdbc/Member.java
@@ -2,21 +2,26 @@ package org.davidmoten.rx.jdbc;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import io.reactivex.subjects.PublishSubject;
+
 final class Member<T> {
 
-    final T value;
-    private final AtomicBoolean inUse = new AtomicBoolean(false);
+	final T value;
+	private final AtomicBoolean inUse = new AtomicBoolean(false);
+	private final PublishSubject<Member<T>> subject;
 
-    public Member(T value) {
-        this.value = value;
-    }
-    
-    public boolean checkout() {
-        return inUse.compareAndSet(false, true); 
-    }
-    
-    public void checkin() {
+	public Member(T value, PublishSubject<Member<T>> subject) {
+		this.value = value;
+		this.subject = subject;
+	}
+
+	public boolean checkout() {
+		return inUse.compareAndSet(false, true);
+	}
+
+	public void checkin() {
         inUse.set(false);
+        subject.onNext(this);
     }
 
 }
--- src/main/java/org/davidmoten/rx/jdbc/Pool.java
+++ src/main/java/org/davidmoten/rx/jdbc/Pool.java
@@ -8,37 +8,20 @@ import io.reactivex.subjects.PublishSubject;
 
 public class Pool<T> {
 
-    private final Callable<T> factory;
-    private final Member<T>[] members;
-    private final PublishSubject<Member<T>> subject = PublishSubject.create();
+	private final Flowable<Member<T>> members;
 
-    @SuppressWarnings("unchecked")
-    public Pool(Callable<T> factory, int maxSize) {
-        this.factory = factory;
-        this.members = (Member<T>[]) new Object[maxSize];
-    }
+	public Pool(Callable<T> factory, int maxSize) {
+		PublishSubject<Member<T>> subject = PublishSubject.create();
+		this.members = Flowable //
+				.range(1, maxSize) //
+				.map(n -> new Member<T>(factory.call(), subject)) //
+				.mergeWith(subject.toFlowable(BackpressureStrategy.BUFFER)) //
+				.share() //
+				.filter(member -> member.checkout());
+	}
 
-    public Flowable<T> members() {
-        return subject.toFlowable(BackpressureStrategy.BUFFER).share().filter(member -> member.checkout()).map(member -> member.value);
-    }
-
-    public boolean checkout() throws Exception {
-        //TODO handle asynchronous calls
-        for (int i = 0; i < members.length; i++) {
-            if (members[i] == null) {
-                members[i] = new Member<T>(factory.call());
-                subject.onNext(members[i]);
-                return true;
-            } else if (members[i].checkout()) {
-                subject.onNext(members[i]);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void checkin(Member<T> member) {
-        member.checkin();
-    }
+	public Flowable<Member<T>> members() {
+		return members;
+	}
 
 }
