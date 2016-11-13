d0ca28e97e2da2632107c3a570a7fdaf7eb55157
Dave Moten <davidmoten@gmail.com>
2016-11-13 17:58:47 +1100

    add Tx

---
+++ src/main/java/org/davidmoten/rx/jdbc/Tx.java
@@ -0,0 +1,22 @@
+package org.davidmoten.rx.jdbc;
+
+import java.sql.Connection;
+
+public interface Tx<T> {
+    
+    boolean isValue();
+
+    boolean isComplete();
+
+    boolean isError();
+
+    T value();
+
+    Throwable throwable();
+
+    Connection connection();
+
+    void commit();
+
+    void rollback();
+}
