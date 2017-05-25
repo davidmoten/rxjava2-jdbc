ef250562ca4389dbfb128cb8cf1ad027e7d0ac51
Dave Moten <davidmoten@gmail.com>
2017-05-25 14:30:14 +1000

    fix warning

--- src/main/java/org/davidmoten/rx/jdbc/exceptions/ColumnIndexOutOfRangeException.java
+++ src/main/java/org/davidmoten/rx/jdbc/exceptions/ColumnIndexOutOfRangeException.java
@@ -2,6 +2,8 @@ package org.davidmoten.rx.jdbc.exceptions;
 
 public final class ColumnIndexOutOfRangeException extends SQLRuntimeException {
 
+    private static final long serialVersionUID = 900639264983427617L;
+
     public ColumnIndexOutOfRangeException(String message) {
         super (message);
     }
