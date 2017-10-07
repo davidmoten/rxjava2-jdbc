fafb7c26779752a2f13cbc6d78284a045aab44b0
Dave Moten <davidmoten@gmail.com>
2017-10-07 09:58:31 +0200

    document the use of Database.from(Single<Connection>, Action) in javadoc #6

--- src/main/java/org/davidmoten/rx/jdbc/Database.java
+++ src/main/java/org/davidmoten/rx/jdbc/Database.java
@@ -35,10 +35,29 @@ public final class Database implements AutoCloseable {
         this.onClose = onClose;
     }
 
-    public static Database from(@Nonnull Single<Connection> connections, @Nonnull Action onClose) {
-        Preconditions.checkNotNull(connections, "connections cannot be null");
+    /**
+     * Creates a Database instance. This is a low-level creation method that needs a
+     * lot of care so it is recommended to use the other static factory methods on
+     * Database that will handle pooling for you.
+     * 
+     * @param connection
+     *            each subscription to this Single should yield a valid open
+     *            Connection to the database. Bear in mind that once a query is
+     *            completed with this connection the connection will be closed. If
+     *            you don't want the connection to be closed (pooling behaviour)
+     *            then you need to override the `close()` behaviour of the
+     *            Connection and handle the semaphore pool pattern yourself (!) or
+     *            preferrably use the other static factory methods like
+     *            {@code Database.from(url, maxPoolSize)} for instance.
+     * @param onClose
+     *            the action that will be run when {@code Database.close()} is
+     *            called.
+     * @return Database instance using the given connection provider
+     */
+    public static Database from(@Nonnull Single<Connection> connection, @Nonnull Action onClose) {
+        Preconditions.checkNotNull(connection, "connections cannot be null");
         Preconditions.checkNotNull(onClose, "onClose cannot be null");
-        return new Database(connections, onClose);
+        return new Database(connection, onClose);
     }
 
     public static Database from(@Nonnull String url, int maxPoolSize) {
