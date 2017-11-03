7d30f9afee5b16b63afcf4e1fe3d141e32209185
Dave Moten <davidmoten@gmail.com>
2017-11-04 07:17:34 +1100

    add stored proc

---
+++ rxjava2-jdbc-stored-procedure/src/main/java/org/davidmoten/rx/jdbc/StoredProcExample.java
@@ -0,0 +1,27 @@
+package org.davidmoten.rx.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class StoredProcExample {
+
+    public static void getPersonCount(int minScore, int[] count) throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:default:connection");
+        try (PreparedStatement stmt = prepareStatement(con, minScore);
+                ResultSet rs = stmt.executeQuery()) {
+            rs.next();
+            count[0] = rs.getInt(1);
+        }
+    }
+
+    private static PreparedStatement prepareStatement(Connection con, int minScore)
+            throws SQLException {
+        PreparedStatement stmt = con.prepareStatement("select count(*) from person where score>?");
+        stmt.setInt(1, minScore);
+        return stmt;
+    }
+
+}
