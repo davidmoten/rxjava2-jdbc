7327d23b01616b0e469a1273b4021c8223501ad7
Dave Moten <davidmoten@gmail.com>
2017-11-04 06:57:14 +1100

    refactor to mulitmodule

--- generate-site.sh
+++ generate-site.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
+cd rxjava2-jdbc
 NAME=${PWD##*/}
 mvn site
-cd ../davidmoten.github.io
+cd ../../davidmoten.github.io
 git pull
 mkdir -p $NAME
-cp -r ../$NAME/target/site/* $NAME/
+cp -r ../$NAME/rxjava2-jdbc/target/site/* $NAME/
 git add .
 git commit -am "update site reports"
 git push
