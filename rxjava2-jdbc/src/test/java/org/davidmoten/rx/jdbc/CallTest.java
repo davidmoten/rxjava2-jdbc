596f6904fdef34b8748f507560c775d56e90dc91
Dave Moten <davidmoten@gmail.com>
2017-11-14 13:17:57 +1100

    add Call test

---
+++ rxjava2-jdbc/src/test/java/org/davidmoten/rx/jdbc/CallTest.java
@@ -0,0 +1,14 @@
+package org.davidmoten.rx.jdbc;
+
+import org.junit.Test;
+
+import com.github.davidmoten.junit.Asserts;
+
+public class CallTest {
+    
+    @Test
+    public void isUtilityClass() {
+        Asserts.assertIsUtilityClass(Call.class);
+    }
+
+}
