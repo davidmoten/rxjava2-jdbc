b5b3c4dc26a96be410533a391c4ad64de4442922
Dave Moten <davidmoten@gmail.com>
2017-11-17 17:57:12 +1100

    simplify SQLRuntimeException

--- rxjava2-jdbc/src/main/java/org/davidmoten/rx/jdbc/exceptions/SQLRuntimeException.java
+++ rxjava2-jdbc/src/main/java/org/davidmoten/rx/jdbc/exceptions/SQLRuntimeException.java
@@ -1,24 +1,15 @@
 package org.davidmoten.rx.jdbc.exceptions;
 
-import java.sql.SQLException;
-
 public class SQLRuntimeException extends RuntimeException {
 
     private static final long serialVersionUID = -3879393806890615797L;
 
-    public SQLRuntimeException(SQLException e) {
+    public SQLRuntimeException(Throwable e) {
         super(e);
-    }
-
-    public SQLRuntimeException() {
-        super();
     }
 
     public SQLRuntimeException(String message) {
         super(message);
     }
 
-    public SQLRuntimeException(Exception e) {
-        super(e);
-    }
 }
