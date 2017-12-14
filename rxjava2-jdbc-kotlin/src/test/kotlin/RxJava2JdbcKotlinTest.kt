f5b028e841528af2159b8c003e35c09eb8a95e33
Dave Moten <davidmoten@gmail.com>
2017-12-14 15:57:41 +1100

    kotlin sample dev

---
+++ rxjava2-jdbc-kotlin/src/test/kotlin/RxJava2JdbcKotlinTest.kt
@@ -0,0 +1,42 @@
+package rx.jdbc.kotlin
+
+import kotlin.test.assertEquals
+import org.junit.Test
+import org.davidmoten.rx.jdbc.Database
+import org.davidmoten.rx.jdbc.annotations.Query
+import org.davidmoten.rx.jdbc.annotations.Column
+import java.util.concurrent.TimeUnit
+
+class RxJava2JdbcTest {
+    @Test 
+    fun testSelect() : Unit {
+        Database
+          .test()
+          .select("select name from person order by name")
+          .getAs(String::class.java)
+          .test()
+          .awaitDone(20, TimeUnit.SECONDS)
+          .assertValues("FRED", "JOSEPH", "MARMADUKE")
+          .assertComplete()
+    }
+
+    @Test
+    fun testAutomap() : Unit {
+        Database
+          .test()
+          .select(Person::class.java)
+          .get()
+          .map({x -> x.name()})
+          .test()
+          .awaitDone(20, TimeUnit.SECONDS)
+          .assertValues("FRED", "JOSEPH", "MARMADUKE")
+          .assertComplete()
+    }
+}
+
+@Query("select name from person order by name")
+interface Person {
+    @Column("name")
+    fun name() : String
+}
+
