a210a8ca2ee7db5672e429ac6c19fca95251b6c1
David Moten <davidmoten@gmail.com>
2021-10-12 09:10:36 +1100

    increase coverage of MemberSingle.tryEmit, not successful in covering activeCount check #58

---
+++ rxjava2-pool/src/test/java/org/davidmoten/rx/pool/NonBlockingPoolConcurrencyTest.java
@@ -0,0 +1,43 @@
+package org.davidmoten.rx.pool;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.junit.Test;
+
+import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
+
+public class NonBlockingPoolConcurrencyTest {
+
+    @Test
+    public void memberSingleCoverage() throws Exception {
+        // attempt to get coverage of x.activeCount > 0 expression in tryEmit
+        // has not been successful but covers the other statements in tryEmit
+        AtomicLong count = new AtomicLong();
+        AtomicLong disposed = new AtomicLong();
+        int poolSize = 4;
+        try (Pool<Long> pool = NonBlockingPool //
+                .factory(() -> count.incrementAndGet()) //
+                .healthCheck(n -> true) //
+                .maxSize(poolSize) //
+                .maxIdleTime(1, TimeUnit.MINUTES) //
+                .disposer(n -> disposed.incrementAndGet()) //
+                .build()) {
+            Flowable //
+                    .rangeLong(0, 100000) //
+                    .flatMapSingle(x -> pool.member(), false, 4) //
+                    .observeOn(Schedulers.from(Executors.newFixedThreadPool(1))) //
+                    .doOnNext(member -> member.checkin()) //
+                    .count() //
+                    .blockingGet();
+            assertEquals(poolSize, count.get());
+            assertEquals(0, disposed.get());
+        }
+        assertEquals(poolSize, disposed.get());
+    }
+
+}
